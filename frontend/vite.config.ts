492723737eac59d8272e85fbf96947d980014925
Nhat Linh Nguyen <linhbaljeet0208@gmail.com>
2026-04-03 09:34:40 +0200

    Fix export port and add allowed domain to vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   base: "",
   server: {
     port: 5173,
+    allowedHosts: ["file.nlnguyen.fr"],
     proxy: {
       "/api": {
         target: process.env.VITE_BACKEND_URL || "http://localhost:2818",
